a4dc08ed7ae1871d2e64b8a35801842f0e064516
alex_gaew <alex_gaew@mail.ru>
2025-11-11 10:31:16 +0300

    1- добавил стартовый пакет

---
+++ hw09/hw09-spring-mvc/src/main/java/ru/otus/hw/dto/BookDto.java
@@ -0,0 +1,15 @@
+package ru.otus.hw.dto;
+
+import java.util.List;
+import ru.otus.hw.models.Book;
+
+public record BookDto(long id, String title, AuthorDto author, List<GenreDto> genres) {
+
+  public static BookDto from(Book book) {
+    return new BookDto(
+        book.getId(),
+        book.getTitle(),
+        AuthorDto.from(book.getAuthor()),
+        GenreDto.from(book.getGenres()));
+  }
+}
